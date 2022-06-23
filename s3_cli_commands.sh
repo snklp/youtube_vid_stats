66cbca855c47a914055e04a449a7a685b3baa13a
sankalp <tmsankalpgo@gmail.com>
2022-06-23 09:10:51 +0530

    ytstatssparkjobnlambda

---
+++ s3_cli_commands.sh
@@ -0,0 +1,16 @@
+# yt-vid-stats-raw-data is the targeted S3 bucket to store raw data from local machine
+
+# To copy all JSON Reference data to same location:
+aws s3 cp . s3://yt-vid-stats-raw-data/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+
+# To copy all CSV data files to its own location, following Hive-style patterns:
+aws s3 cp CAvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://yt-vid-stats-raw-data/raw_statistics/region=us/
